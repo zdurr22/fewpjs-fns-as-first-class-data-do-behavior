df46343d62b2473ec79bf403b926d653244b9560
ihollander <ianholla@gmail.com>
2021-08-24 17:53:44 -0400

    Allow innerText values in test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,22 +1,27 @@
 describe("index.js", () => {
   describe("greet() function", () => {
     it("Returns Good Morning before 12pm", () => {
-      expect(greet("8:40")).to.equal('Good Morning')
-    })
+      expect(greet("8:40")).to.equal("Good Morning");
+    });
     it("Returns Good Afternoon between 12pm and 5pm ", () => {
-      expect(greet("14:21")).to.equal('Good Afternoon')
-    })
+      expect(greet("14:21")).to.equal("Good Afternoon");
+    });
     it("Returns Good Evening after 5pm", () => {
-      expect(greet("19:00")).to.equal('Good Evening')
-    })
-  })
+      expect(greet("19:00")).to.equal("Good Evening");
+    });
+  });
 
   describe("displayMessage() function", () => {
     it("updates the DOM with an appropriate message", () => {
-      let testContent = "TEST"
-      displayMessage(testContent)
-      let content = document.getElementById("greeting").textContent
-      expect(content).to.equal(testContent)
-    })
-  })
-})
+      let testContent = "TEST";
+      displayMessage(testContent);
+
+      // The test library doesn't treat innerText and textContent the same, so we need to check for both
+      // https://github.com/jsdom/jsdom/issues/1245
+      let content = greetingEl.innerText
+        ? greetingEl.innerText
+        : greetingEl.textContent;
+      expect(content).to.equal(testContent);
+    });
+  });
+});
